f0d1af41dbd799a95b11267edcb95f3d9e2456e1
Khalid Jameer <kjameer0@gmail.com>
2023-10-08 00:40:19 -0400

    got mui working with css modules

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react';
 import { Outlet } from 'react-router';
 import './App.css';
 import { Card } from './component/Card/card';
-import globalStyles from './globals.module.css';
+import ProjectCard from './component/ProjectCard/ProjectCard';
+import { StyledEngineProvider } from '@mui/material/styles';
+
 function App() {
   return (
-    <>
-      <Card className={globalStyles.} />
-      <div>hi</div>
+    <StyledEngineProvider injectFirst>
+      <Card />
+      <ProjectCard />
       <Outlet />
-    </>
+    </StyledEngineProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ErrorPage } from "./ErrorPage.jsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Card } from "./component/Card/card.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ErrorPage } from './ErrorPage.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 const router = createBrowserRouter([
   {
-    path: "",
+    path: '',
     element: <App />,
     error: <ErrorPage />,
     children: [],
   },
 ]);
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
