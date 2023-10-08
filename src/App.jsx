96183b45b843ca8d39e8f0f8b4c86b6086579104
Raymond Broome <rbroome@gmail.com>
2023-10-08 13:07:34 -0400

    update sidebar layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { useState } from 'react';
 import { Outlet } from 'react-router';
 import './App.css';
@@ -7,11 +6,15 @@ import ProjectCard from './component/ProjectCard/ProjectCard';
 import NavBar from './component/NavBar/NavBar';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { bountiesWithRequirements } from '../data/bounty';
+import { Sidebar } from './component/Sidebar/Sidebar';
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <NavBar />
-      <ProjectCard data={bountiesWithRequirements[0]} />
+      <div className='flex-container'>
+        <Sidebar />
+        <ProjectCard data={bountiesWithRequirements[0]} />
+      </div>
       <Outlet />
     </StyledEngineProvider>
   );
--- src/component/Sidebar/sidebar.jsx
+++ src/component/Sidebar/sidebar.jsx
@@ -1,29 +1,24 @@
-export function Filter() {
+import styles from '/src/component/Sidebar/Sidebar.module.css';
+
+export function Sidebar() {
     return (
-        <aside>
-            <div className="">
-                <div className="form-item">
-                    <label>Search:</label>
+        <aside className={styles.sidebarContainer}>
+            <div>
+                <div className={styles.formItem}>
+                    <label>Search</label>
                     <input type="text" name="location" />
-                    <button>Search</button>
                 </div>
-                <div className="form-item">
-                    <label>Location:</label>
+                <div className={styles.formItem}>
+                    <label>Location</label>
                     <input type="text" name="location" placeholder="Borough or Zip Code" />
                 </div>
-                <div className="form-item">
-                    <label>Category:</label>
-                    <select>
-                        <option value="water">Water</option>
-                        <option value="air">Air</option>
-                        <option value="waste">Waste</option>
-                        <option value="energy">Energy</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
-                <div className="form-item">
-                    <label>Show Completed?</label>
-                    <input type="checkbox" name="show-completed" />
+                <div className={styles.formItem}>
+                    <label>Category</label>
+                    <input type="checkbox" value="water" /> Water
+                    <input type="checkbox" value="air" /> Air
+                    <input type="checkbox" value="waste" />Waste
+                    <input type="checkbox" value="energy" />Energy
+                    <input type="checkbox" value="other" />Other
                 </div>
             </div>
         </aside>
